65cd5836071b75d53285884076cd41220d331f1f
Edwin Kayang <epkayang@gmail.com>
2026-01-12 22:02:24 -0700

    completed local variables

--- c/debug.c
+++ c/debug.c
@@ -14,6 +14,12 @@ static int simpleInstruction(const char *name, int offset) {
   return offset + 1;
 }
 
+static int byteInstruction(const char *name, Chunk *chunk, int offset) {
+  uint8_t slot = chunk->code[offset + 1];
+  printf("%-16s %4d\n", name, slot);
+  return offset + 2;
+}
+
 static int constantInstruction(const char *name, Chunk *chunk, int offset) {
   uint8_t constant = chunk->code[offset + 1];
   printf("%-16s %4d '", name, constant);
@@ -44,6 +50,8 @@ int disassembleInstruction(Chunk *chunk, int offset) {
     return simpleInstruction("OP_TRUE", offset);
   case OP_FALSE:
     return simpleInstruction("OP_FALSE", offset);
+  case OP_GET_GLOBAL:
+    return constantInstruction("OP_GET_GLOBAL", chunk, offset);
   case OP_DEFINE_GLOBAL:
     return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
   case OP_SET_GLOBAL:
@@ -70,6 +78,10 @@ int disassembleInstruction(Chunk *chunk, int offset) {
     return simpleInstruction("OP_PRINT", offset);
   case OP_POP:
     return simpleInstruction("OP_POP", offset);
+  case OP_GET_LOCAL:
+    return byteInstruction("OP_GET_LOCAL", chunk, offset);
+  case OP_SET_LOCAL:
+    return byteInstruction("OP_SET_LOCAL", chunk, offset);
   default:
     printf("Unknown opcode %d\n", instruction);
     return offset + 1;
